813c36bf22a3f8812593f52b8859e4b4106d5902
sasss77 <saugatpaneru40@gmail.com>
2025-06-14 17:47:19 +0545

    converted the homepage css into tailwind

--- Inkspira/src/main.jsx
+++ Inkspira/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import LoginRes from './Login and register/LoginRes.jsx'
+import { BrowserRouter} from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
   
-  </StrictMode>,
+  </BrowserRouter>,
 )
