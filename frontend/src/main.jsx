0df247d3d43798b61488b1278a13a1113b3573c2
sasss77 <saugatpaneru40@gmail.com>
2025-07-29 10:14:53 +0545

    dependencise updated + react router dom in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,11 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-
-import { BrowserRouter } from 'react-router-dom';
-
-
-
-createRoot(document.getElementById('root')).render(
- <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
 )
