0757bfb4f4c58de43316d57c40fc2594ae79bcfe
sasss77 <saugatpaneru40@gmail.com>
2025-07-30 13:21:08 +0545

    small updates

--- Backend/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, loginUser } = require("../controller/auth/authController");
-
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,7 @@
-const express = require("express");
+const app = require('./App');
+const { db } = require('./config/db');
 
-const app = express();
-
-const PORT = 6969;
-
-require("dotenv").config();
-
-const userRoutes = require("./routes/userRoute");
-const productRoutes = require("./routes/productRoute");
-const authRoute = require("./routes/authRoute")
-
-app.use(express.json());
-app.use("/api", userRoutes);
-app.use("/api", productRoutes);
-app.use("/api/auth", authRoute);
-
-app.listen(PORT, () => {
-  console.log("Connected");
+const PORT = process.env.PORT || 3000;
+db().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
